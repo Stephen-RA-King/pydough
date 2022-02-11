bcc944479f6db7b594aba388a3500a60053b04fe
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-02-11 18:14:15 +0000

    Add coverage pragma

--- {{cookiecutter.project_slug}}/tests/test_cli.py
+++ {{cookiecutter.project_slug}}/tests/test_cli.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
 from click.testing import CliRunner
 
 from src.{{ cookiecutter.pkg_name }} import cli
@@ -20,7 +22,6 @@ def test_author_verbose():
     assert result.output == 'Author name: {{ cookiecutter.author_name }}\n' \
                             'Author eMail: {{ cookiecutter.email }}\n'
 
-
 def test_author_help():
     runner = CliRunner()
     result = runner.invoke(cli.author, ["--help"])
@@ -33,6 +34,23 @@ def test_pkg_info():
     result = runner.invoke(cli.pkg_info)
     assert result.exit_code == 0
     assert result.output == 'Package Version: {{ cookiecutter.version }}\n'
+
+
+def test_pkg_info_verbose():
+    runner = CliRunner()
+    result = runner.invoke(cli.pkg_info, ["--verbose"])
+    assert result.exit_code == 0
+    assert result.output == "Package Version: {{ cookiecutter.version }}\n" \
+                            "{{ cookiecutter.pkg_name }}\n" \
+                            "{{ cookiecutter.project_short_description }}\n"
+
+
+def test_python_version():
+    version = "{0.major}.{0.minor}.{0.micro}".format(sys.version_info)
+    runner = CliRunner()
+    result = runner.invoke(cli.python)
+    assert result.exit_code == 0
+    assert result.output == f"{version}\n"
 
 
 def test_pkg_info_help():
