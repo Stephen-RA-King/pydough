de3b0ee48cfec6eca7d90572aa41928414ef6902
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-05-16 18:45:40 +0100

    Add docstring to functions

--- {{cookiecutter.project_slug}}/src/{{ cookiecutter.pkg_name }}/{{ cookiecutter.pkg_name }}.py
+++ {{cookiecutter.project_slug}}/src/{{ cookiecutter.pkg_name }}/{{ cookiecutter.pkg_name }}.py
@@ -4,6 +4,24 @@ from typing import Any
 
 
 def fizzbuzz(number_range: int) -> list:
+    """Return integers 1 to N, but print “Fizz” if an integer is divisible by 3,
+    “Buzz” if an integer is divisible by 5, and “FizzBuzz” if an integer is
+    divisible by both 3 and 5
+
+    Parameters
+    ----------
+    number_range: int
+        The maximum number that will be used
+
+    Returns
+    -------
+    list
+        The result will be returned as a list
+
+    Examples
+    --------
+    >>> fizzbuzz(20)
+    """
     result: list[Any] = []
     for num in range(1, number_range):
         if num % 15 == 0:
@@ -18,6 +36,22 @@ def fizzbuzz(number_range: int) -> list:
 
 
 def fibonacci(number_range: int) -> list:
+    """series of numbers in which each number is the sum of the two that precede it
+
+    Parameters
+    ----------
+    number_range: int
+        The maximum number that will be used
+
+    Returns
+    -------
+    list
+        The result will be returned as a list
+
+    Examples
+    --------
+    >>> fibonacci(20)
+    """
     result: list = []
     a, b = 1, 1
     while True:
