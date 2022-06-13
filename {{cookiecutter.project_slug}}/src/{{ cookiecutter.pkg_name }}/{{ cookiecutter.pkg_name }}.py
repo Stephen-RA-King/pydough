640e0d1ff1ce7e5e9fecd34b31c95e4f05d3650d
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-06-13 17:10:41 +0100

    use importlib.resource to get yml config from src

--- {{cookiecutter.project_slug}}/src/{{ cookiecutter.pkg_name }}/{{ cookiecutter.pkg_name }}.py
+++ {{cookiecutter.project_slug}}/src/{{ cookiecutter.pkg_name }}/{{ cookiecutter.pkg_name }}.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 # Core Library modules
+{%- if cookiecutter.use_logging == 'y' %}
+import logging.config
+from importlib import resources{% endif %}
 from typing import Any
 
+# Third party modules
+{%- if cookiecutter.use_logging == 'y' %}
+import yaml  # type: ignore{% endif %}
+
+{%- if cookiecutter.use_logging == 'y' %}
+config = resources.read_text("{{ cookiecutter.pkg_name }}", "logging_config.yaml")
+logging.config.dictConfig(yaml.safe_load(config))
+logger = logging.getLogger("customlogger")
+{% endif %}
 
 def fizzbuzz(number_range: int) -> list:
     """Return integers 1 to N, but print “Fizz” if an integer is divisible by 3,
@@ -32,6 +44,8 @@ def fizzbuzz(number_range: int) -> list:
             result.append("Fizz")
         else:
             result.append(num)
+    {%- if cookiecutter.use_logging == 'y' %}
+    logger.debug(f'fizzbuzz result: {result}'){% endif %}
     return result
 
 
@@ -56,6 +70,8 @@ def fibonacci(number_range: int) -> list:
     a, b = 1, 1
     while True:
         if a >= number_range:
+            {%- if cookiecutter.use_logging == 'y' %}
+            logger.debug(f'fibonacci result: {result}'){% endif %}
             return result
         result.append(a)
         a, b = b, (a + b)
