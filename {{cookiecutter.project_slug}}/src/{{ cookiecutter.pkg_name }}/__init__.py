b1fd8853b6cc7ef6765d14b209bf29ef91e8c59e
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-09-04 18:47:55 +0100

    refactor: migrate deprecated importlib.resources functions

--- {{cookiecutter.project_slug}}/src/{{ cookiecutter.pkg_name }}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{ cookiecutter.pkg_name }}/__init__.py
@@ -1,17 +1,28 @@
 """Top-level package for {{ cookiecutter.pkg_name }}."""
-
-{%- if cookiecutter.use_logging == 'y' %}
-import logging.config
-{% endif %}
-from importlib import resources
-{%- if cookiecutter.config_file == 'json' or cookiecutter.config_file == 'all' %}
-import json{% endif %}
+# Core Library modules
 {%- if cookiecutter.config_file == 'ini' or cookiecutter.config_file == 'all' %}
 import configparser{% endif %}
+{%- if cookiecutter.config_file == 'json' or cookiecutter.config_file == 'all' %}
+import json{% endif %}
+{%- if cookiecutter.use_logging == 'y' %}
+import logging.config{% endif %}
+{%- if cookiecutter.config_file != 'none' or cookiecutter.resource_file != 'none' %}
+from importlib.resources import files, as_file{% endif %}
+
+# Third party modules
 {%- if cookiecutter.config_file == 'toml' or cookiecutter.config_file == 'all' %}
 import toml  # type: ignore{% endif %}
 {%- if cookiecutter.use_logging == 'y' or cookiecutter.config_file == 'yaml' or cookiecutter.config_file == 'all' %}
 import yaml  # type: ignore{% endif %}
+
+__title__ = "{{ cookiecutter.project_name }}"
+__version__ = "{{ cookiecutter.version }}"
+__author__ = "{{ cookiecutter.author_name }}"
+__description__ = "{{ cookiecutter.project_short_description }}"
+__email__ = "{{ cookiecutter.email }}"
+__license__ = "{{ cookiecutter.license }}"
+__copyright__ = "Copyright {% now 'local', '%Y' %} {{ cookiecutter.author_name }}"
+
 
 {% if cookiecutter.use_logging == 'y' %}
 LOGGING_CONFIG = """
@@ -50,32 +61,33 @@ logger = logging.getLogger("init")
 {% endif -%}
 
 {%- if cookiecutter.config_file == 'yaml' or cookiecutter.config_file == 'all' %}
-_yaml_text = resources.read_text("{{ cookiecutter.pkg_name }}", "config.yaml")
-yaml_config = yaml.safe_load(_yaml_text)
+source = files("{{ cookiecutter.pkg_name }}.resources").joinpath('config.yaml')
+with as_file(source) as _yaml_path:
+    _yaml_text = _yaml_path.read_text()
+    yaml_config = yaml.safe_load(_yaml_text)
 {% endif -%}
 
 {%- if cookiecutter.config_file == 'json' or cookiecutter.config_file == 'all' %}
-_json_text = resources.read_text("{{ cookiecutter.pkg_name }}", "config.json")
-json_config = json.loads(_json_text)
+source = files("{{ cookiecutter.pkg_name }}.resources").joinpath('config.json')
+with as_file(source) as _json_path:
+    _json_text = _json_path.read_text()
+    json_config = json.loads(_json_text)
 {% endif -%}
 
 {%- if cookiecutter.config_file == 'ini' or cookiecutter.config_file == 'all' %}
-_ini_text = resources.read_text("{{ cookiecutter.pkg_name }}", "config.ini")
-_ini = configparser.ConfigParser()
-_ini.optionxform = str  # type: ignore
-_ini.read_string(_ini_text)
-ini_config = {section: dict(_ini.items(section)) for section in _ini.sections()}
+source = files("{{ cookiecutter.pkg_name }}.resources").joinpath('config.ini')
+with as_file(source) as _ini_path:
+    _ini_text = _ini_path.read_text()
+    _ini = configparser.ConfigParser()
+    _ini.optionxform = str  # type: ignore
+    _ini.read_string(_ini_text)
+    ini_config = {section: dict(_ini.items(section)) for section in _ini.sections()}
 {% endif -%}
 
 {%- if cookiecutter.config_file == 'toml' or cookiecutter.config_file == 'all' %}
-_toml_text = resources.read_text("{{ cookiecutter.pkg_name }}", "config.toml")
-toml_config = toml.loads(_toml_text)
+source = files("{{ cookiecutter.pkg_name }}.resources").joinpath('config.toml')
+with as_file(source) as _toml_path:
+    _toml_text = _toml_path.read_text()
+    toml_config = toml.loads(_toml_text)
 {% endif %}
 
-__title__ = "{{ cookiecutter.project_name }}"
-__version__ = "{{ cookiecutter.version }}"
-__author__ = "{{ cookiecutter.author_name }}"
-__description__ = "{{ cookiecutter.project_short_description }}"
-__email__ = "{{ cookiecutter.email }}"
-__license__ = "{{ cookiecutter.license }}"
-__copyright__ = "Copyright {% now 'local', '%Y' %} {{ cookiecutter.author_name }}"
